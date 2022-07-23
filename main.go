8415ea5f6ffec934c48248009167984b5695a7cb
youfenghit <109840551+youfenghit@users.noreply.github.com>
2022-07-23 13:20:36 +0800

    rename

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "远坂凛", "Set default nickname.")
+	adana := flag.String("n", "", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -224,7 +224,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "赤い悪魔", "遠坂凛"),
+		NickName:      append([]string{*adana}, "遠坂凛"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
